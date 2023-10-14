c33661e389511ed9e3177732ddba00711fdebb55
JackNugget4 <Nguyenjack4444@gmail.com>
2023-10-14 15:54:45 -0400

    spring cycle 1

---
+++ src/Pages/Home.js
@@ -0,0 +1,40 @@
+import React from 'react'
+import './Styles/Home.css'
+import './Components/Button.css'
+import {useNavigate} from 'react-router-dom'
+import bg from './Images/background.jpg'
+import raff from './Images/raff.png'
+
+export default function Home() {
+
+    const navigate = useNavigate();
+
+    function handleClick(e) {
+        navigate('/Survey')
+    }
+
+    return (
+        <div>
+            <header>
+                <center>
+                <img src={bg} class='bg' />
+                    <div><br></br></div>
+                    <div class='box'>
+                        <h1 class='typed'>Welcome to Raffs Safari</h1>
+                        <img src={raff} class='raff' />
+                        <h2>Hi! I'm Raff and I am here to assist you in your travels!</h2>
+                        <button class='button' onClick={handleClick}>Get Started</button>
+                    </div>
+                </center>
+            </header>
+
+            <footer>
+                <br></br>
+                <br></br>
+                <a href='/Contact' class='foot'>Contact</a>
+                <br></br>
+                <a href='/AboutUs'class='foot'>About Us</a>
+            </footer>
+        </div>
+    )
+}
---
+++ src/Pages/Navbar.js
@@ -0,0 +1,18 @@
+import react from 'react'
+import './Styles/Navbar.css'
+
+export default function Navbar() {
+
+    return (
+        <div class='nav'>
+                <br></br>
+                <a href='/Home' class='alist'>Home</a>
+                <br></br>
+                <a href='/Survey' class='alist'>Survey</a>
+                <br></br>
+                <a href='/Itinerary' class='alist'>Itinerary</a>
+                <br></br>
+                <br></br>
+        </div>
+    )
+}
