8f3d4d7cec45c794540612856a41db13de017efb
Mclovin7291 <100216705+Mclovin7291@users.noreply.github.com>
2023-11-23 00:00:30 -0500

    login/logout button changes

--- src/Pages/Login.js
+++ src/Pages/Login.js
@@ -1,9 +1,63 @@
-import React from 'react';
+import React, { useEffect, useState } from 'react';
 import { GoogleLogin } from "@react-oauth/google";
 import { GoogleOAuthProvider } from '@react-oauth/google';
 import jwt_decode from "jwt-decode";
 
 
+function Login() {
+  const [user, setUser] = useState({});
+  const google = window.google
+
+  //get JWT ID
+  function handleCallBackResponse(response){
+    console.log("Encoded JWT ID token: "+ response.credential);
+    var userObject = jwt_decode(response.credential);
+    console.log(userObject);
+    setUser(userObject);
+    document.getElementById("signInDiv").hidden = true;
+  }
+
+  function handleSignOut(event){
+    setUser({});
+    document.getElementById("signInDiv").hidden = false;
+  }
+
+  useEffect(() => {
+  google.accounts.id.initialize({
+    client_id: "1005325099127-qdh6mv05e08eqtqhb6v4o31t0vb9m43d.apps.googleusercontent.com",
+    callback: handleCallBackResponse
+  });
+
+  google.accounts.id.renderButton(
+    document.getElementById("signInDiv"),
+    {theme: "outline", size: "large"}
+  )
+
+  //google.accounts.id.prompt();
+  }, []);
+
+  //if no user: show sign in button
+  //if we have a user: show the log out button
+  return( 
+    <div className="login">
+      <div id="signInDiv"></div>
+      { Object.keys(user).length != 0 &&
+          <button onClick={ (e) => handleSignOut(e)}>Sign Out</button>
+      }
+      
+      { user &&
+        <div>
+        <img src={user.picture}></img>
+        <h3>{user.name}</h3>
+        </div>
+      }
+    </div>
+  );
+
+}
+export default Login;
+
+/*
 function Login() {
   return (
     <GoogleLogin
@@ -21,3 +75,5 @@ function Login() {
 }
 
 export default Login;
+
+*/
