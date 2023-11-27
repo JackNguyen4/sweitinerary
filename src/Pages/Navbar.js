a23fb39e71e38c10f2317e339cdb21013487add8
Mclovin7291 <100216705+Mclovin7291@users.noreply.github.com>
2023-11-26 19:19:23 -0500

    Got rid of the uber button on the navbar.

--- src/Pages/Navbar.js
+++ src/Pages/Navbar.js
@@ -14,7 +14,6 @@ export default function Navbar() {
                 <br></br>
                 <a href='/Maps' class='alist'>Maps</a>
                 <br></br>
-                <a href='/Uber' class='alist'>Uber</a>
                 <br></br>
         </div>
     )
